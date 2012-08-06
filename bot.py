57cff04f9f84f151c159be6990bd5d88ae02f774
waawal <waawal@boom.ws>
2012-08-06 21:13:59 +0200

    3.4- not yet

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ CLASSIFIERS = frozenset(("Programming Language :: Python :: 3",
                          "Programming Language :: Python :: 3.1",
                          "Programming Language :: Python :: 3.2",
                          "Programming Language :: Python :: 3.3",
-                         "Programming Language :: Python :: 3.4",
+#                        "Programming Language :: Python :: 3.4",
                          ))
 
 
