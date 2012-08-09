184ecd26c6121602e21a19254fe819241bbbc6ce
waawal <waawal@boom.ws>
2012-08-09 23:48:39 +0200

    unified strings

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-""" Message-bot running on heroku, tweets all new packages that supports py3k
+''' Message-bot running on heroku, tweets all new packages that supports py3k
     (checks for updates of old <3k packages also)
-"""
+'''
 
 
 import os
@@ -13,48 +13,48 @@ from twitter import OAuth, Twitter
 
 QUERY_INTERVAL = 2 * 60 # In seconds, intervals between queries to PYPI, 2 min.
 PYPI_SERVICE = 'http://pypi.python.org/pypi'
-CLASSIFIERS = frozenset(("Programming Language :: Python :: 3",
-                         "Programming Language :: Python :: 3.0",
-                         "Programming Language :: Python :: 3.1",
-                         "Programming Language :: Python :: 3.2",
-                         "Programming Language :: Python :: 3.3",
+CLASSIFIERS = frozenset(('Programming Language :: Python :: 3',
+                         'Programming Language :: Python :: 3.0',
+                         'Programming Language :: Python :: 3.1',
+                         'Programming Language :: Python :: 3.2',
+                         'Programming Language :: Python :: 3.3',
                          ))
 
 
 def count_chars_of_tweet(tweet):
-    """ Counts the characters within a tweet.
-    """
+    ''' Counts the characters within a tweet.
+    '''
     chars = len(tweet) # we initially append the future spaces between words
     for part in tweet:
-        if part.startswith("http://", "https://"):
+        if part.startswith('http://', 'https://'):
             chars += 21 # t.co-urls are at most 21
         else:
             chars += len(part)
     return chars
 
 def post_to_twitter(projectname, meta, msgtype):
-    """ Composes a twitter-post and sends it on its way.
-    """
-    DIVIDER = "-"
+    ''' Composes a twitter-post and sends it on its way.
+    '''
+    DIVIDER = '-'
     message = []
 
-    msgtype = '[{0}]'.format(msgtype.upper())
+    msgtype = "[{0}]".format(msgtype.upper())
     message.append(msgtype)
 
     message.append(projectname)
     message.append(DIVIDER)
 
     message.append('pypi:')
-    message.append('http://pypi.python.org/pypi/{0}/'.format(projectname))
+    message.append("http://pypi.python.org/pypi/{0}/".format(projectname))
 
     # If a home-page is provided, lets use it, otherwise - fall back to crate.
     homepage = meta.get('home_page', 'UNKNOWN')
-    if not homepage == "UNKNOWN":
+    if not homepage == 'UNKNOWN':
         message.append('www:')
         message.append(homepage)
     else:
         message.append('crate.io:')
-        message.append('https://crate.io/packages/{0}/'.format(projectname))
+        message.append("https://crate.io/packages/{0}/".format(projectname))
 
     # Important, add #python
     message.append('#python')
@@ -64,7 +64,7 @@ def post_to_twitter(projectname, meta, msgtype):
     chrsleft = 140 - (currentchars + 1) # +1 = the space before summary.
     
     summary = meta.get('summary', 'UNKNOWN')
-    if not summary == "UNKNOWN":
+    if not summary == 'UNKNOWN':
         if len(summary) > chrsleft:
             message.insert(2, "".join((summary[:(chrsleft-3)], '...'))) # Trunc.
         else:
@@ -82,11 +82,12 @@ def post_to_twitter(projectname, meta, msgtype):
     twitter.statuses.update(status=finalmessage)
 
 def check_for_updates(supported, classifiers, interval, service):
-    """ Checks for new projects and updates.
-    """
+    ''' Checks for new projects and updates.
+    '''
     startprocessing = time() # Let's do this!
     client = xmlrpclib.ServerProxy(service)
-    updates = client.changelog(int(startprocessing - interval))
+    since = int(startprocessing - interval)
+    updates = client.changelog(since)
     # Returns a list of:
     #['vimeo', '0.1.2', 1344087619,'update description, classifiers']
     
@@ -115,9 +116,9 @@ def check_for_updates(supported, classifiers, interval, service):
     return processingtime
 
 def get_supported(classifiers, service):
-    """ Builds a set of the PYPI-projects currently listed under the provided
+    ''' Builds a set of the PYPI-projects currently listed under the provided
         classifiers.
-    """
+    '''
     client = xmlrpclib.ServerProxy(service)
     multicall = xmlrpclib.MultiCall(client)
     [multicall.browse([classifier]) for classifier in classifiers]
